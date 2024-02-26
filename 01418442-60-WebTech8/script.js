62daa85d665da3c49fda85189940b93c13ae9afa
namesangjun_ <namedarrius@gmail.com>
2024-02-26 12:04:02 +0700

    Update script.js

--- 01418442-60-WebTech8/script.js
+++ 01418442-60-WebTech8/script.js
@@ -1,35 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
-    loadMenuItem()
-})
+document.addEventListener("DOMContentLoaded", function () {
+	LoadMenuItems();
+});
 
-function loadMenuItem() {
-    console.log('menu is loading');
-    fetch('menu.json').then((response)=> response.json())
-    .then((menuItem)=> {
-        const menuContainer = document.querySelector(".menu");
-        menuContainer.innerHTML = "";
+function LoadMenuItems() {
+	console.log("menu is loading");
+	fetch("menu.json")
+		.then((response) => response.json())
+		.then((menuItems) => {
+			const menuContainer = document.querySelector(".menu");
+			menuContainer.innerHTML = "";
 
-        menuItem.forEach(item => {
-            const menuItemElement = document.createElement("div")
-            menuItemElement.classList.add("menu-item");
-            menuItemElement.innerHTML = `
-            <img src="${item.imgSrc}" alt="${item.name}" />
-            <h2>${item.name}</h2>
-            <p>${item.price}</p>`
-            menuContainer.appendChild(menuItemElement);
+			menuItems.forEach((item) => {
+				const menuItemElement = document.createElement("div");
+				menuItemElement.classList.add("menu-item");
+				menuItemElement.innerHTML = `
+                <img src="${item.imgSrc}" alt="${item.name}" srcset="" />
+				<h2>${item.name}</h2>
+				<p>${item.price}</p>
+                `;
+				menuContainer.appendChild(menuItemElement);
 
-            menuItemElement.addEventListener('click', function() {
-                let orders = JSON.parse(localStorage.getItem('customerOrders')) || [];
-                console.log(orders);
-                const existingOrderIndex = orders.findIndex(
-                    (order) => order.name === item.name
-                );
-                console.log(existingOrderIndex);
-                orders.push({ name: item.name, price: item.price});
-                console.log(orders);
+				menuItemElement.addEventListener("click", function () {
+					let orders = JSON.parse(localStorage.getItem("customerOrders")) || [];
+					const existingOrderIndex = orders.findIndex(
+						(order) => order.name === item.name
+					);
 
-                localStorage.setItem("customerOrders", JSON.stringify(orders));
-            });
-        });
-    });
+					if (existingOrderIndex == -1) {
+						orders.push({ name: item.name, price: item.price, quantity: 1 });
+					} else {
+						orders[existingOrderIndex].quantity += 1;
+					}
+					console.log(orders);
+
+					localStorage.setItem("customerOrders", JSON.stringify(orders));
+				});
+			});
+		});
 }
