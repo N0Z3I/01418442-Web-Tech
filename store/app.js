8cfa366e96aae743665dde75d4c5f03cb9650caa
namesangjun_ <namedarrius@gmail.com>
2024-03-04 14:00:04 +0700

    Updated

---
+++ store/app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+
+const express = require('express');
+
+// middleware
+const app = express();
+
+app.use((request, response, next) => {
+    console.log('Say Wi from middleware');
+    next()
+});
+
+app.use((request, response, next) => {
+    console.log('Say Fi from another middleware');
+    response.send('<h1>This is WiFi<h1>')
+});
+
+const server = http.createServer(app);
+
+server.listen(3000);
